b6365b2ae521599a64b429ccf2925c3cc98922bf
Toni Toplek <ttoplek@foi.hr>
2019-07-30 07:44:12 +0200

    v6.9.9 - Posložena kartica robe

--- Code-iT Ugostiteljstvo/Code-iT Ugostiteljstvo/PC CAFFE/bin/Debug/DBbackup/DBbackup.bat
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-pg_dump.exe --host localhost --port 5432 --username postgres --format custom --blobs --verbose --file "C:\Users\Toni\Desktop\Sigurnosna kopija baze 2019\db2019-07-18 12.backup" "miva2019"
-=======
-pg_dump.exe --host localhost --port 5432 --username postgres --format custom --blobs --verbose --file "C:\Users\Korisnik\Desktop\Sigurnosna kopija baze 2019\db2019-07-19.backup" "db2019"
->>>>>>> 18c2e5467139031a7445ece1df40cdbb84ae8c6e
-=======
-pg_dump.exe --host localhost --port 5432 --username postgres --format custom --blobs --verbose --file "C:\Users\Korisnik\Desktop\Sigurnosna kopija baze 2019\db2019-07-22.backup" "db2019"
->>>>>>> 83f1f0042aca98b69c8c19680e031d67281833af
